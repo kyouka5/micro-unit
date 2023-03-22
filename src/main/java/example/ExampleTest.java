eb9167edb627531292e92df0d26afba732e6a41f
Márta Nagy <kyoukaa98@gmail.com>
2023-03-22 18:22:26 +0100

    add project from week 4

--- src/main/java/example/ExampleTest.java
+++ src/main/java/example/ExampleTest.java
@@ -14,30 +14,35 @@ public class ExampleTest {
     }
 
     @Test
-    public void a() {
+    public void test_alwaysTrueCondition_shouldReturnTrue() {
         assertTrue(1 + 1 == 2, "This should always be true");
     } // success
 
     @Test
-    public void b() {
+    public void test_parseInt_shouldThrowException_whenParameterIsInvalid() {
+        Integer.parseInt("Hello, world!");
+    } // error
+
+    @Test
+    public void test_alwaysFalseCondition_shouldReturnFalse() {
         assertTrue(1 + 1 == 0, "This should never be true");
     } // failure
 
     @Test(expected = IOException.class)
-    public void c() throws Exception {
+    public void test_emptyTestMethod_shouldThrowException() throws Exception {
     } // failure
 
     @Test(expected = IOException.class)
-    public void d() throws Exception {
+    public void test_expectedIOException_shouldThrowException() throws Exception {
         throw new IOException("An I/O exception occurred");
     } // success
 
     @Test(expected = IOException.class)
-    public void e() {
+    public void test_differentException_shouldThrowException() {
         throw new RuntimeException("Oops!");
     } // error
 
-    public void f() {
+    public void test_nonTestMethod_shouldNotBeExecuted() {
     }
 
     public static void main(String[] args) throws Exception {
